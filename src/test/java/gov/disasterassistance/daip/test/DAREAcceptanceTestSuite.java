192b0eee0b3dcc777a85fa2f50469ca1e8b2e046
jwei22 <jimmy.wei22@gmail.com>
2016-06-28 10:34:19 -0400

    Verify twitter feed and local resource scripts

--- src/test/java/gov/disasterassistance/daip/test/DAREAcceptanceTestSuite.java
+++ src/test/java/gov/disasterassistance/daip/test/DAREAcceptanceTestSuite.java
@@ -2,4 +2,4 @@ package gov.disasterassistance.daip.test;
 
 import net.serenitybdd.jbehave.SerenityStories;
 
-public class DAREAcceptanceTestSuite extends SerenityStories{}
+public class DAREAcceptanceTestSuite extends SerenityStories {}
---
+++ src/test/java/gov/disasterassistance/daip/test/exceptions/FeedException.java
@@ -0,0 +1,12 @@
+package gov.disasterassistance.daip.test.exceptions;
+
+public class FeedException extends Exception{
+	//private static final long serialVersionUID = 1L;
+	
+	public FeedException() {
+		super();
+	}
+	public FeedException(String message) {
+		super(message);
+	}
+}
